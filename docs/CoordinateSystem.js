0264850164705c25288e12883d6889e047fb3afb
Ciaxur <omar41097@gmail.com>
2018-10-29 18:57:50 -0400

    Updated Github Site

--- docs/CoordinateSystem.js
+++ docs/CoordinateSystem.js
@@ -20,39 +20,44 @@ class CoordinateSystem {
      * @param resolution The Spacing Resolution between the axies
      */
     constructor(coordWidth, coordHeight, resolution) {
-        // Set Default Values
-        this.data = [];
+        // Set Default Data Values
+        this.vectorData = [];
+        this.pointsData = [];
+        this.polynomialData = [];
+        // Setup Triangle Default Properties
         this.triWidth = 5;
         this.triHeight = 1;
         // Setup Coordinate System Dimensions
         this.coordHeight = coordHeight;
         this.coordWidth = coordWidth;
         this.spacing = resolution === undefined ? 40 : resolution;
+        // Setup Identity Matrix of System Dimensions
+        this.originX = Math.floor(this.coordWidth / 2);
+        this.originY = Math.floor(this.coordHeight / 2);
     }
     /** Draws Coordinate System */
     drawCoordinateSystem() {
-        const midX = Math.floor(this.coordWidth / 2);
-        const midY = Math.floor(this.coordHeight / 2);
+        // Start Drawing
         ctx.save();
         ctx.strokeStyle = "rgb(255,255,255)";
         ctx.lineWidth = 1.5;
         // Draw x-axis
         ctx.beginPath();
-        ctx.lineTo(midX, this.coordHeight);
-        ctx.lineTo(midX, 0);
+        ctx.lineTo(this.originX, this.coordHeight);
+        ctx.lineTo(this.originX, 0);
         ctx.stroke();
         ctx.closePath();
         // Draw y-axis
         ctx.beginPath();
-        ctx.lineTo(0, midY);
-        ctx.lineTo(this.coordWidth, midY);
+        ctx.lineTo(0, this.originY);
+        ctx.lineTo(this.coordWidth, this.originY);
         ctx.stroke();
         ctx.closePath();
         // Change Properties
         ctx.strokeStyle = "rgb(255,255,255, 0.2)";
         ctx.lineWidth = 0.2;
         // Draw x-axis (Right-Side) Dashes
-        for (let x = midX; x < this.coordWidth; x += this.spacing) {
+        for (let x = this.originX; x < this.coordWidth; x += this.spacing) {
             ctx.beginPath();
             ctx.lineTo(x, this.coordHeight);
             ctx.lineTo(x, 0);
@@ -60,7 +65,7 @@ class CoordinateSystem {
             ctx.closePath();
         }
         // (Left-Side) Dashes
-        for (let x = midX; x > 0; x -= this.spacing) {
+        for (let x = this.originX; x > 0; x -= this.spacing) {
             ctx.beginPath();
             ctx.lineTo(x, this.coordHeight);
             ctx.lineTo(x, 0);
@@ -68,7 +73,7 @@ class CoordinateSystem {
             ctx.closePath();
         }
         // Draw y-axis (Up-Side) Dashes
-        for (let y = midY; y > 0; y -= this.spacing) {
+        for (let y = this.originY; y > 0; y -= this.spacing) {
             ctx.beginPath();
             ctx.lineTo(0, y);
             ctx.lineTo(this.coordWidth, y);
@@ -76,7 +81,7 @@ class CoordinateSystem {
             ctx.closePath();
         }
         // Draw y-axis (Down-Side) Dashes
-        for (let y = midY; y < this.coordHeight; y += this.spacing) {
+        for (let y = this.originY; y < this.coordHeight; y += this.spacing) {
             ctx.beginPath();
             ctx.lineTo(0, y);
             ctx.lineTo(this.coordWidth, y);
@@ -84,13 +89,10 @@ class CoordinateSystem {
             ctx.closePath();
         }
         ctx.restore();
-        // Draw the Vectors on top of the Coordinate System
-        for (const m of this.data) { // Get all Matrix Objects
-            for (const vec of m.data) { // Break down all the Vectors inside each Object
-                // Draw Each Vector
-                this.drawVectors(midX, midY, vec);
-            }
-        }
+        // DRAW ALL DATA
+        this.drawVectors();
+        this.drawPoints();
+        this.drawPolynomials();
     }
     /** Resize Coordinate System
      *
@@ -107,25 +109,42 @@ class CoordinateSystem {
      * @param y The y-axis Value of the Vector
      */
     addVector(x, y) {
-        // Check if Matrix Object
-        if (x instanceof Matrix) {
-            // Check if m*2 Matrix
-            if (x.getColumns() !== 2 && x.getRows() !== 2) {
-                console.error("Invalid Matrix Dimensions! Only m by 2 Matricies are allowed!");
-                return;
+        // Add to Vector Data
+        this.add_Mx2(x, y, "vector");
+    }
+    /** Adds a Point to the System | Matrix Object of Dimension m*2
+     *
+     * @param x The x-axis Value of the Point or a Matrix Object
+     * @param y The y-axis Value of the Point
+     */
+    addPoint(x, y) {
+        // Add to Points Data
+        this.add_Mx2(x, y, "point");
+    }
+    /** Adds a Polynomial to the System | Matrix Object of Dimension m*2
+     *
+     * @param m The m Value of the Polynomial "m" (mx + b) or a Matrix Object for [a, b, c, ...] for polynomial (ex. ax^2 + bx + c)
+     * @param b The y Value of the Polynomial "b" | (mx + b)
+     */
+    addPolynomial(m, b) {
+        // Adjust Matrix Polynomial Data
+        if (m instanceof Matrix) {
+            // Make sure only (mx1) or (1xn) Shapes ALLOWED
+            if (m.getColumns() !== 1 && m.getRows() !== 1) {
+                console.error("Polynomial Error - Invalid Matrix Shape! Only mx1 or 1xn Shapes Accepted!");
             }
-            // Transpose from [2,1] to [1,2]
-            if (x.getColumns() !== 2) {
-                x.transpose();
+            // Add to Data if Valid Shape
+            else {
+                // Check if (1xn) to Transpose it
+                if (m.getRows() !== 1) {
+                    m.transpose();
+                }
+                this.polynomialData.push(m);
             }
-            this.data.push(x);
         }
-        // Create a Matrix Object with Set Values
+        // Add to Linear Data
         else {
-            const m = new Matrix(1, 2);
-            y = y !== undefined ? y : 0;
-            m.set([[x, y]]);
-            this.data.push(m);
+            this.add_Mx2(m, b, "polynomial");
         }
     }
     /** Sets Vector Pointer's Dimensions (Size)
@@ -143,9 +162,11 @@ class CoordinateSystem {
         this.triWidth = width;
         this.triHeight = height;
     }
-    /** Resets Vector data | Removes all data */
+    /** Resets All data | Removes all data */
     reset() {
-        this.data = [];
+        this.vectorData = [];
+        this.pointsData = [];
+        this.polynomialData = [];
     }
     /** Draw the Projection Matrix onto Coordinate System
      * @param projMatrix Projection Matrix (Matrix Object) of Dimensions [2,2]
@@ -219,44 +240,101 @@ class CoordinateSystem {
         // Return Result 2D Vector (1x2)
         return Matrix.fromArray([[x, y]]);
     }
-    /** Draw Vectors onto the Coordinate System
-     *
-     * @param originX The Origin X-Axis Point
-     * @param originY The Origin Y-Axis Point
-     * @param vec 2DVector Array to Draw
-     */
-    drawVectors(originX, originY, vec) {
-        // START DRAWING
-        ctx.save();
-        ctx.strokeStyle = "rgb(0, 200, 0)";
-        ctx.fillStyle = ctx.strokeStyle;
-        ctx.lineWidth = 2;
-        ctx.beginPath();
-        // DRAW VECTOR LINE
-        const x = originX + (vec[0] * this.spacing);
-        const y = originY - (vec[1] * this.spacing);
-        const magnitude = CoordinateSystem.calculateMagnitude(vec[0], vec[1]);
-        const theta = CoordinateSystem.calculateAngle(vec[0], vec[1]);
-        ctx.lineTo(originX, originY);
-        ctx.lineTo(x, y);
-        ctx.stroke();
-        ctx.closePath();
-        // DRAW POINTER TRIANGLE
-        const tX1 = originX + (((magnitude - this.triHeight) * Math.cos(theta)) * this.spacing);
-        const tY1 = originY - (((magnitude - this.triHeight) * Math.sin(theta)) * this.spacing);
-        const tX2 = tX1 + (this.triWidth * Math.cos(theta + (Math.PI / 2)));
-        const tY2 = tY1 - (this.triWidth * Math.sin(theta + (Math.PI / 2)));
-        const tX3 = tX1 + (this.triWidth * Math.cos(theta - (Math.PI / 2)));
-        const tY3 = tY1 - (this.triWidth * Math.sin(theta - (Math.PI / 2)));
-        ctx.beginPath();
-        ctx.lineTo(tX1, tY1);
-        ctx.lineTo(tX2, tY2);
-        ctx.lineTo(x, y);
-        ctx.lineTo(tX3, tY3);
-        ctx.lineTo(tX1, tY1);
-        ctx.fill();
-        ctx.closePath();
-        ctx.restore();
+    /** Draw all Vectors onto the Coordinate System */
+    drawVectors() {
+        for (const m of this.vectorData) { // Get all Matrix Objects
+            for (const vec of m.data) { // Break down all the Vectors inside each Object
+                // START DRAWING
+                ctx.save();
+                ctx.strokeStyle = "rgb(0, 200, 0)";
+                ctx.fillStyle = ctx.strokeStyle;
+                ctx.lineWidth = 2;
+                ctx.beginPath();
+                // DRAW VECTOR LINE
+                const x = this.originX + (vec[0] * this.spacing);
+                const y = this.originY - (vec[1] * this.spacing);
+                const magnitude = CoordinateSystem.calculateMagnitude(vec[0], vec[1]);
+                const theta = CoordinateSystem.calculateAngle(vec[0], vec[1]);
+                ctx.lineTo(this.originX, this.originY);
+                ctx.lineTo(x, y);
+                ctx.stroke();
+                ctx.closePath();
+                // DRAW POINTER TRIANGLE
+                const tX1 = this.originX + (((magnitude - this.triHeight) * Math.cos(theta)) * this.spacing);
+                const tY1 = this.originY - (((magnitude - this.triHeight) * Math.sin(theta)) * this.spacing);
+                const tX2 = tX1 + (this.triWidth * Math.cos(theta + (Math.PI / 2)));
+                const tY2 = tY1 - (this.triWidth * Math.sin(theta + (Math.PI / 2)));
+                const tX3 = tX1 + (this.triWidth * Math.cos(theta - (Math.PI / 2)));
+                const tY3 = tY1 - (this.triWidth * Math.sin(theta - (Math.PI / 2)));
+                ctx.beginPath();
+                ctx.lineTo(tX1, tY1);
+                ctx.lineTo(tX2, tY2);
+                ctx.lineTo(x, y);
+                ctx.lineTo(tX3, tY3);
+                ctx.lineTo(tX1, tY1);
+                ctx.fill();
+                ctx.closePath();
+                ctx.restore();
+            }
+        }
+    }
+    /** Draw all Points as Circle onto the Coordinate System */
+    drawPoints() {
+        for (const m of this.pointsData) { // Get all Matrix Objects
+            for (const vec of m.data) { // Break down all the Vectors inside each Object
+                // Create Variables (For Readability)
+                const x = this.originX + (vec[0] * this.spacing);
+                const y = this.originY - (vec[1] * this.spacing);
+                const r = 4;
+                // START DRAWING
+                ctx.save();
+                ctx.beginPath();
+                ctx.fillStyle = "rgb(0, 255, 0)";
+                ctx.ellipse(x, y, r, r, 1, 0, 2 * Math.PI);
+                ctx.fill();
+                ctx.closePath();
+                ctx.restore();
+            }
+        }
+    }
+    /** Draws all Polynomials onto Coordiante System */
+    drawPolynomials() {
+        // Variables used
+        const maxX = Math.floor(this.coordWidth / this.spacing) / 2;
+        // POLY DATA
+        for (const polyData of this.polynomialData) { // Get all Matrix Objects
+            for (const vec of polyData.data) { // Break down all the Vectors inside each Object
+                // Start Drawing Path
+                ctx.save();
+                ctx.beginPath();
+                // Setup Properties
+                ctx.strokeStyle = "rgb(0, 255, 0)";
+                // To Save Time, check if vec is only 2 (Linear = mx + b)
+                if (vec.length === 2) {
+                    const m = vec[0];
+                    const b = vec[1];
+                    const x1 = this.adjustToOrigin(maxX, false);
+                    const y1 = this.adjustToOrigin(m * maxX + b, true);
+                    const x2 = this.adjustToOrigin(-maxX, false);
+                    const y2 = this.adjustToOrigin(m * (-maxX) + b, true);
+                    ;
+                    ctx.lineTo(x1, y1);
+                    ctx.lineTo(x2, y2);
+                }
+                // Organize & Draw the Polynomial
+                else {
+                    for (let i = -maxX; i < maxX; i += 1) {
+                        const result = CoordinateSystem.solvePolynomialY(i, vec);
+                        const y = this.adjustToOrigin(result, true);
+                        const x = this.adjustToOrigin(i, false);
+                        ctx.lineTo(x, y);
+                    }
+                }
+                ctx.stroke();
+                ctx.closePath();
+                ctx.restore();
+            }
+        }
     }
     /** Checks if Matrix is a Valid 2D Projection Matrix
      *
@@ -288,6 +366,71 @@ class CoordinateSystem {
         // console.log((matArr[1][0] * matArr[0][0]) + (matArr[1][1] * matArr[1][0]), (matArr[1][0] * matArr[0][1]) + (matArr[1][1] * matArr[1][1]));
         return true;
     }
+    /** Adds a mx2 or 1xn Matrix to it's Data depending on given Type
+     *
+     * @param x The x-axis Value | Matrix Object
+     * @param y The y-axis Value
+     * @param dataType Data Type that the Matrix will be added/created to
+     */
+    add_Mx2(x, y, dataType) {
+        // Check if Matrix Object
+        if (x instanceof Matrix) {
+            // Check if m*2 Matrix
+            if (x.getColumns() !== 2 && x.getRows() !== 2) {
+                console.error("Invalid Matrix Dimensions! Only m by 2 Matricies are allowed!");
+                return;
+            }
+            // Transpose from [2,n] to [n,2]
+            if (x.getColumns() !== 2) {
+                x.transpose();
+            }
+            // Push Data Depending on Type
+            if (dataType === "vector")
+                this.vectorData.push(x);
+            else if (dataType === "point")
+                this.pointsData.push(x);
+            else if (dataType === "polynomial")
+                this.polynomialData.push(x); // mx2 | Added here prior
+        }
+        // Create a Matrix Object with Set Values
+        else {
+            // Create Data Depending on Type
+            if (dataType === "vector")
+                this.vectorData.push(Matrix.fromArray([[x, y]]));
+            else if (dataType === "point")
+                this.pointsData.push(Matrix.fromArray([[x, y]]));
+            else if (dataType === "polynomial")
+                this.polynomialData.push(Matrix.fromArray([[x, y]])); // Plynomial: Simple Linear
+        }
+    }
+    /** Draws a Circle with radius "5" at given Coordinates
+     *
+     * @param x X-Axis Coordinate Point
+     * @param y Y-Axis Coordinate Point
+     */
+    drawCirc(x, y) {
+        // Adjust Values to Origin
+        x = this.adjustToOrigin(x, false);
+        y = this.adjustToOrigin(y, true);
+        // Draw
+        ctx.save();
+        ctx.fillStyle = "rgb(255, 0, 0)";
+        ctx.beginPath();
+        ctx.ellipse(x, y, 5, 5, 0, 0, 2 * Math.PI);
+        ctx.closePath();
+        ctx.fill();
+        ctx.restore();
+    }
+    /** Adjusts value Relative to Origin
+     *
+     * @param n The Value to Adjust
+     * @param isY Boolean stating if Value is for Y-Axis or not
+     */
+    adjustToOrigin(n, isY) {
+        return isY
+            ? this.originY - (n * this.spacing)
+            : this.originX + (n * this.spacing);
+    }
     // STATIC METHODS
     /** Calculates the Magnitude of the Vector
      *
@@ -310,6 +453,22 @@ class CoordinateSystem {
             return Math.atan(y / x) - Math.PI;
         }
         return Math.atan(y / x);
+    }
+    /** Solves for any Polynomial for "y"
+     *
+     * @param x The x Varaible that will be multiplied by the polynomial
+     * @param polyArr The Polynomial Array for Value of each "Increase in Power" [1,2,3] -> [1x^2, 2x, 3] -> 1x^2 + 2x + 3
+     */
+    static solvePolynomialY(x, polyArr) {
+        // Variables used
+        let y = polyArr[polyArr.length - 1]; // Y-Initial Set to the last Element (Since not mult. by X)
+        let power = 1; // Initial Power set to power of 1
+        // Go through all Polynomial Values
+        for (let i = polyArr.length - 2; i >= 0; i--) {
+            y += polyArr[i] * Math.pow(x, power++); // Accumulate the Caculation of x^power * Value
+        }
+        // Return y Result
+        return y;
     }
 }
 //# sourceMappingURL=CoordinateSystem.js.map
