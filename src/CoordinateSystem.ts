144fd225741012a0d25a6cb625cc700cd6fb8363
Ciaxur <omar41097@gmail.com>
2018-10-29 19:01:15 -0400

    Fixed Resize Issue

--- src/CoordinateSystem.ts
+++ src/CoordinateSystem.ts
@@ -139,6 +139,10 @@ class CoordinateSystem {
     public resize(newWidth: number, newHeight: number): void {
         this.coordWidth = newWidth;
         this.coordHeight = newHeight;
+
+        // Update Origin Value
+        this.originX = Math.floor(this.coordWidth / 2);
+        this.originY = Math.floor(this.coordHeight / 2);
     }
 
     /** Adds a Vector to the System | Matrix Object of Dimension m*2
