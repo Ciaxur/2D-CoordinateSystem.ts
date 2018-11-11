2a811ceb958f38992bce35e4da0f819dcb998046
Ciaxur <omar41097@gmail.com>
2018-11-11 18:22:33 -0500

    Implemeted the Menu Class

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,8 @@ const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d");
 let WIDTH = 0;
 let HEIGHT = 0;
-let CoordSystem;
+let CoordSystem: CoordinateSystem;
+let CoordMenu: Menu;
 
 
 // On Load
@@ -16,9 +17,12 @@ document.addEventListener("DOMContentLoaded", () => {
     canvas.height = HEIGHT = window.innerHeight;
 
 
-    // Setup
+    // Setup Coordinate System
     CoordSystem = new CoordinateSystem(WIDTH, HEIGHT, 10);
     CoordSystem.addVector(10, 10);
+
+    // Setup Main Menu for Coordinate System
+    CoordMenu = new Menu(CoordSystem, ctx);
 
     // Adding a Matrix Object of m by 2 Dimensions
     CoordSystem.addVector(
@@ -31,6 +35,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
         ]));
 
+    
+    // Add Mouse Clicked Event after Setting Up
+    window.addEventListener("click", mouseClickAction);
+    
+    
     // Init Drawing
     draw();
 });
@@ -54,8 +63,49 @@ function background(r: number, g: number, b: number) {
 function draw() {
     background(0, 0, 0);
 
+    // Draw the Coordinate System
     CoordSystem.drawCoordinateSystem();
+
+    // Draw the Menu ontop of the Coordinate System
+    CoordMenu.start();
 
     window.requestAnimationFrame(draw);
 }
 
+
+
+/** Function call when Mouse click Event */
+function mouseClickAction(e: MouseEvent): void {
+    // Menu Button Data
+    const menuBtnPos = CoordMenu.getButtonPosition();
+    const btnShape = CoordMenu.getButtonShape();
+
+    // Close Menu Button Data
+    const closeBtnPos = CoordMenu.getCloseMenuBtnPosition();
+    const closeBtnShape = CoordMenu.getCloseMenuBtnShape();
+
+    // Mouse Data
+    const mousePos: Position2D = {
+        x: e.x, y: e.y
+    };
+
+
+    // Menu Button vs Mouse
+    // Check if Mouse within Menu Button Click | Trigger Menu
+    if (Menu.within(menuBtnPos, btnShape, mousePos) && !CoordMenu.getMenuStatus()) {
+        CoordMenu.triggerMenu();
+    }
+
+    // Menu Box vs Mouse
+    // Check if Mouse within Menu Buttons
+    // Check if Mouse within Close Menu Button
+    else {
+        // Check Close Button
+        if (Menu.within(closeBtnPos, closeBtnShape, mousePos)) {
+            CoordMenu.triggerMenu();
+        }
+
+        // Check Menu Buttons
+        CoordMenu.clickedButton(mousePos);
+    }
+}
